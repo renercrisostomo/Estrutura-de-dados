229f0773e9ba491591ca942a03f888478c9584ef
Renêr <rener.bluernr@gmail.com>
2023-09-15 20:34:14 +0000

    suporte para adicionar listas nas pilhas

--- pilhas/pilhas-simples.py
+++ pilhas/pilhas-simples.py
@@ -3,26 +3,31 @@ class Node:
     self.data = data
     self.next = next
   
-class Stack(list):
+class Stack():
 
-  def __init__(self, list):
+  def __init__(self):
     self.length = 0
     self.head = None
   
   def add(self, data):
-    newNode = Node()
-    newNode.data = data
-    if self.length == 0:
-      self.head = newNode
-    else:
-      newNode.next = self.head
-      self.head = newNode
-    self.length += 1
+    count = 0
+    dataLength = (len(data) if type(data) == list else 1)
+      
+    while count < dataLength:
+      newNode = Node()
+      newNode.data = (data[count] if type(data) == list else data)
+      if self.length == 0:
+        self.head = newNode
+      else:
+        newNode.next = self.head
+        self.head = newNode
+      self.length += 1
+      count += 1
 
   def pop(self):
     if self.length != 0:
       self.head = self.head.next
-      self.length -=1
+      self.length -= 1
 
   def print(self):
     if self.length != 0:
@@ -30,11 +35,14 @@ class Stack(list):
       current = self.head
       while current != None:        
         print("Node %d has value %s"%(pos, current.data))
-        pos +=1
+        pos += 1
         if pos == self.length : return
         current = current.next
 
 pilha = Stack()
 
+pilha.add([3,4,5])
+
 pilha.print()
+pilha.pop()
 pilha.print()
